a6172b1fbe7f61b3b9bcd6e2188965fb11d7a674
Daniil Galtsev <galtsev.daniil@gmail.com>
2023-10-21 08:25:52 +0000

    unfix gosec error

--- json.go
+++ json.go
@@ -27,9 +27,5 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	_, err = w.Write(dat)
-	if err != nil {
-		w.WriteHeader(500)
-		return
-	}
+	w.Write(dat)
 }
